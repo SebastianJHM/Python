0b5461001d6382c9d96e5fa3c66463d2e3c3083c
SebastianJHM <53499619+SebastianJHM@users.noreply.github.com>
2020-09-17 18:21:10 -0500

    flowshop

--- renombrar.py
+++ renombrar.py
@@ -5,9 +5,9 @@ import sys
 def main(argv): 
     path = "C:\\Users\\USUARIO1\\Desktop\\Escritura 2386\\"
     for count, filename in enumerate(os.listdir(path)): 
-        nombre ="Esc.2386 Pág" + str(count+1) + ".jpeg"
-        src =path + filename 
-        dst =path + nombre
+        nombre = "Esc.2386 Pág" + str(count+1) + ".jpeg"
+        src = path + filename 
+        dst = path + nombre
         os.rename(src, dst)
     #rof
 #fed
